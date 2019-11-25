da4037c94165eee0688a52af916ca709e6718442
qazi112 <qarsalan13@gmail.com>
2019-11-25 05:42:57 -0800

    Commiting hashing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-const int tableSize=9;
+const int tableSize=10;
 class Hashing{
 public:
     int counter;
-    int hashTable[9];
+    int hashTable[10];
     Hashing()
     {
         counter=0;
@@ -15,7 +15,7 @@ public:
     }
    int hash(int key);
     void insert(int key);
-    void delete(int key);
+    void delete_Key(int key);
     bool isFull();
     bool isEmpty();
     int search(int key);
@@ -23,19 +23,90 @@ public:
 
 bool Hashing::isFull()
 {
-    if(counter==9) return true;
+    if(counter==10)
+        return true;
+    else {
+        return false;
+    }
 }
 bool Hashing::isEmpty()
 {
-    if(counter==0) return true;
+    if(counter==0)
+        return true;
+    else {
+        return false;
+    }
 }
 void Hashing::insert(int key)
 {
+    if(isFull()==true)
+    {
+        cout<<"Table Full \n";
+    }
+    else{
+
+        int i=1;
+        int index=hash(key);
+        while(hashTable[index]!=-1)
+        {
+            index=(hash(key)+i)%tableSize;
+            i++;
+        }
+        hashTable[index]=key;
+        counter++;
+        }
+
+}
+int Hashing::search(int key)
+{
     int i=1;
     int index=hash(key);
-    while(hashTable[index]!=)
+    if(hashTable[index]==key)
+    {
+        cout<<"Found : At : "<<endl;
+        return index;
+    }
+    else{
+        while(hashTable[index]!=key || hashTable[index]!=-1)
+        {
+            index=(hash(key)+i)%tableSize;
+            i++;
+
+        }
+    if(hashTable[index]==key)
+    {
+        cout<<"Found : At :\n";
+        return index;
+    }
+    else {
+        cout<<"Not Found\n";
+        return -1;
+    }
+}
+}
+void Hashing::delete_Key(int key)
+{
+    if(isEmpty()==true)
+    {
+        cout<<"Hash Table Is Empty\n";
+        return;
+    }
+    else{
+        int index=search(key);
+        if(index!=-1)
+        {
+            hashTable[index]=-1;
+            counter--;
+            return;
+        }
+    }
 }
 int Hashing::hash(int key)
 {
     return key%tableSize;
+}
+int main()
+{
+    return 0;
+
 }
